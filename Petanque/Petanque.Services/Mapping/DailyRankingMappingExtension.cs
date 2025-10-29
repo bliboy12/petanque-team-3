017c818dc9bcef32d731dc537b3c95b7eb207e29
Lucas Delaunoit <lucasdelaunoit@gmail.com>
2025-10-29 23:36:13 +0100

    Add: Adding all the mapping extensions, adding the missing id's of the models & adding comments above all properties of the models to be able to know how variables were translated

---
+++ Petanque/Petanque.Services/Mapping/DailyRankingMappingExtension.cs
@@ -0,0 +1,61 @@
+using Petanque.Contracts.Requests;
+using Petanque.Contracts.Responses;
+using Petanque.Models;
+using Petanque.Services.Exceptions;
+using Petanque.Storage;
+
+namespace Petanque.Services.Mapping;
+
+public static class DailyRankingMappingExtension
+{
+    public static DailyRankingModel AsModel(this Dagklassement dailyRanking)
+    {
+        return new DailyRankingModel
+        {
+            Id = dailyRanking.DagklassementId,
+            MatchDayId = dailyRanking.SpeeldagId,
+            MatchDay = dailyRanking.Speeldag.AsModel(),
+            PlayerId = dailyRanking.SpelerId,
+            Player = dailyRanking.Speler.AsModel(),
+            MainPoints = dailyRanking.Hoofdpunten,
+            PlusMinPoints = dailyRanking.PlusMinPunten,
+        };
+    }
+
+    public static DailyRankingModel AsModel(this DagKlassementRequestContract dailyRanking)
+    {
+        return new DailyRankingModel
+        {
+            MatchDayId = dailyRanking.SpeeldagId,
+            PlayerId = dailyRanking.SpelerId,
+            MainPoints = dailyRanking.Hoofdpunten,
+            PlusMinPoints = dailyRanking.PlusMinPunten
+        };
+    }
+
+    public static DagKlassementResponseContract AsContract(this DailyRankingModel dailyRanking)
+    {
+        return new DagKlassementResponseContract
+        {
+            DagklassementId = dailyRanking.Id ?? throw new MappingException(),
+            SpeeldagId = dailyRanking.MatchDayId,
+            SpelerId = dailyRanking.PlayerId,
+            Hoofdpunten = dailyRanking.MainPoints,
+            PlusMinPunten = dailyRanking.PlusMinPoints
+        };
+    }
+
+    public static Dagklassement AsEntity(this DailyRankingModel dailyRanking)
+    {
+        return new Dagklassement
+        {
+            DagklassementId = dailyRanking.Id ?? throw new MappingException(),
+            SpeeldagId = dailyRanking.MatchDayId,
+            SpelerId = dailyRanking.PlayerId,
+            Hoofdpunten = dailyRanking.MainPoints,
+            PlusMinPunten = dailyRanking.PlusMinPoints,
+            Speeldag = dailyRanking.MatchDay.AsEntity(),
+            Speler = dailyRanking.Player.AsEntity()
+        };
+    }
+}
---
+++ Petanque/Petanque.Services/Mapping/GameMappingExtension.cs
@@ -0,0 +1,62 @@
+using Petanque.Contracts.Requests;
+using Petanque.Contracts.Responses;
+using Petanque.Models;
+using Petanque.Services.Exceptions;
+using Petanque.Storage;
+
+namespace Petanque.Services.Mapping;
+
+public static class GameMappingExtension
+{
+    public static GameModel AsModel(this Spel game)
+    {
+        return new GameModel
+        {
+            Id = game.SpelId,
+            MatchDayId = game.SpeeldagId,
+            Terrain = game.Terrein,
+            ScoreA = game.ScoreA,
+            ScoreB = game.ScoreB,
+            MatchDay = game.Speeldag.AsModel(),
+            GameDistributions = game.Spelverdelings.Select(g => g.AsModel()).ToList()
+        };
+    }
+
+    public static GameModel AsModel(this SpelRequestContract game)
+    {
+        return new GameModel
+        {
+            MatchDayId = game.SpeeldagId,
+            Terrain = game.Terrein,
+            ScoreA = game.ScoreA,
+            ScoreB = game.ScoreB
+        };
+    }
+
+    public static SpelResponseContract AsContract(this GameModel game)
+    {
+        return new SpelResponseContract
+        {
+            SpelId = game.Id ?? throw new MappingException(),
+            SpeeldagId = game.MatchDayId,
+            Terrein = game.Terrain,
+            ScoreA = game.ScoreA,
+            ScoreB = game.ScoreB,
+            Spelverdelingen = game.GameDistributions.Select(g => g.AsContract()).ToList()
+        };
+    }
+
+    public static Spel AsEntity(this GameModel game)
+    {
+        return new Spel
+        {
+            SpelId = game.Id ?? throw new MappingException(),
+            SpeeldagId = game.MatchDayId,
+            Terrein = game.Terrain,
+            ScoreA = game.ScoreA,
+            ScoreB = game.ScoreB,
+            Speeldag = game.MatchDay.AsEntity(),
+            Spelverdelings = game.GameDistributions.Select(g => g.AsEntity()).ToList()
+        };
+    }
+}
